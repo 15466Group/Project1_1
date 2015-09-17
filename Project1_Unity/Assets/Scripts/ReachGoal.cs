a03388ff5a6b97472af3b1a3f0a6424b52e151fe
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2015-09-17 17:52:54 -0400

    edited filenames, added Reachgoal skely

---
+++ Project1_Unity/Assets/Scripts/ReachGoal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReachGoal : MonoBehaviour {
+
+	public GameObject target;
+
+	private Vector3 acceleration;
+	private Vector3 velocity;
+	private float accMag;
+	private float speedMax;
+
+	private float lambda; //angle from target to character
+	private float psi; //direction character is currently facing
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
